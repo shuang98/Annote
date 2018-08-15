3d533bac91a16cb62d958aa667687549bca9e866
stanley <stanleyhuang83@gmail.com>
2018-08-15 15:08:53 -0700

    express to react

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ var documentsRouter = require('./routes/documents');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// use react
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,8 +19,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/api', documentsRouter);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
